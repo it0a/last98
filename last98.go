53d5c846cf8b92062f05f01ca010576473d276d3
it0a <recursive404@gmail.com>
2014-11-08 13:36:16 -0500

    Adding bootstrap

--- last98.go
+++ last98.go
@@ -1,62 +1,45 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"github.com/gorilla/mux"
+	"html/template"
+	"log"
 	"net/http"
 )
 
+type Page struct {
+	Title string
+}
+
+var templates = template.Must(template.ParseFiles("header.html"))
+
 func main() {
-	router := mux.NewRouter().StrictSlash(false)
-	do_route(router)
-	fmt.Println("Starting server on :8080")
-	http.ListenAndServe(":8080", router)
+	var host = flag.String("host", "127.0.0.1", "IP of host to run web server on")
+	var port = flag.Int("port", 8080, "Port to run webserver on")
+	var staticPath = flag.String("staticPath", "static/", "Path to static files")
+	flag.Parse()
+	//
+	router := mux.NewRouter()
+	router.HandleFunc("/", IndexHandler)
+	router.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir(*staticPath))))
+	//
+	addr := fmt.Sprintf("%s:%d", *host, *port)
+	log.Printf("Listening on %s", addr)
+	//
+	err := http.ListenAndServe(addr, router)
+	if err != nil {
+		log.Fatal("ListenAndServe error: ", err)
+	}
 }
 
-func do_route(router *mux.Router) {
-	do_images_route(router)
-}
-
-func do_images_route(router *mux.Router) {
-	images := router.Path("/").Subrouter()
-	images.Methods("GET").HandlerFunc(imagesIndexHandler)
-	images.Methods("POST").HandlerFunc(imagesCreateHandler)
-	do_images_singular_route(router)
-}
-
-func do_images_singular_route(router *mux.Router) {
-	image := router.PathPrefix("/{id}").Subrouter()
-	image.Methods("GET").Path("/edit").HandlerFunc(imageEditHandler)
-	image.Methods("GET").HandlerFunc(imageShowHandler)
-	image.Methods("PUT", "POST").HandlerFunc(imageUpdateHandler)
-	image.Methods("DELETE").HandlerFunc(imageDeleteHandler)
-}
-
-func HomeHandler(rw http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(rw, "Home")
-}
-
-func imagesIndexHandler(rw http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(rw, "images index")
-}
-
-func imagesCreateHandler(rw http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(rw, "images create")
-}
-
-func imageShowHandler(rw http.ResponseWriter, r *http.Request) {
-	id := mux.Vars(r)["id"]
-	fmt.Fprintln(rw, "showing image", id)
-}
-
-func imageUpdateHandler(rw http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(rw, "image update")
-}
-
-func imageDeleteHandler(rw http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(rw, "image delete")
-}
-
-func imageEditHandler(rw http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(rw, "image edit")
+func IndexHandler(response http.ResponseWriter, request *http.Request) {
+	log.Printf("Handling request with IndexHandler")
+	response.Header().Set("Content-type", "text/html")
+	err := request.ParseForm()
+	if err != nil {
+		http.Error(response, fmt.Sprintf("Error parsing url %v", err), 500)
+	}
+	templates.ExecuteTemplate(response, "header", Page{Title: "Home"})
 }
